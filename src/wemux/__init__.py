7a1d4920e9734cada4b376a98ab6a29e3949303e
donsprallo <donsprallo@gmail.com>
2024-10-11 12:49:11 +0200

    Add ruff as dev dependency

--- src/wemux/__init__.py
+++ src/wemux/__init__.py
@@ -1,20 +1,20 @@
-from .dispatcher import CommandDispatcher
-from .dispatcher import CommandDispatcherFunc
-from .dispatcher import EventDispatcher
-from .dispatcher import EventDispatcherFunc
-from .dispatcher import InMemoryCommandDispatcher
-from .dispatcher import InMemoryEventDispatcher
-from .errors import HandlerNotFoundError
-from .errors import MessageBusError
-from .handler import CommandHandler
-from .handler import EventListener
-from .message import Command
-from .message import Event
-from .message import Message
-from .message import Result
-from .messagebus import MessageBus
-from .middleware import LoggerMiddleware
-from .middleware import Middleware
-from .stream import EventStream
-from .stream import EventStreamReader
-from .stream import InMemoryEventStreamReader
+from .dispatcher import CommandDispatcher as CommandDispatcher
+from .dispatcher import CommandDispatcherFunc as CommandDispatcherFunc
+from .dispatcher import EventDispatcher as EventDispatcher
+from .dispatcher import EventDispatcherFunc as EventDispatcherFunc
+from .dispatcher import InMemoryCommandDispatcher as InMemoryCommandDispatcher
+from .dispatcher import InMemoryEventDispatcher as InMemoryEventDispatcher
+from .errors import HandlerNotFoundError as HandlerNotFoundError
+from .errors import MessageBusError as MessageBusError
+from .handler import CommandHandler as CommandHandler
+from .handler import EventListener as EventListener
+from .message import Command as Command
+from .message import Event as Event
+from .message import Message as Message
+from .message import Result as Result
+from .messagebus import MessageBus as MessageBus
+from .middleware import LoggerMiddleware as LoggerMiddleware
+from .middleware import Middleware as Middleware
+from .stream import EventStream as EventStream
+from .stream import EventStreamReader as EventStreamReader
+from .stream import InMemoryEventStreamReader as InMemoryEventStreamReader
